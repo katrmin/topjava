de70dc98f9edee19d8ff1acafd99ff8f655e1fdd
katrmin <katrmin@yandex.ru>
2021-10-21 00:00:04 +0300

    HW1 - 4 point

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,11 +1,40 @@
 package ru.javawebinar.topjava.service;
 
 import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 @Service
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(Meal Meal, Integer userId) {
+        return repository.save(Meal, userId);
+    }
+
+    public void delete(int id) {
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    public Meal get(int id, Integer userId) {
+        return checkNotFoundWithId(repository.get(id, userId), id);
+    }
+
+
+    public List<Meal> getAll() {
+        return (List<Meal>) repository.getAll();
+    }
+
+    public void update(Meal Meal, Integer userId) {
+        checkNotFoundWithId(repository.save(Meal, userId), Meal.getId());
+    }
 }
