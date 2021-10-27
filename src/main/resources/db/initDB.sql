3603557fba36761bb966f6a60bcd9364457723bf
katrmin <katrmin@yandex.ru>
2021-10-27 04:33:22 +0300

    HW3 - create table meals in initDB.sql

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -18,8 +18,17 @@ CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
 CREATE TABLE user_roles
 (
-    user_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL  ,
     role    VARCHAR,
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS meals;
+CREATE TABLE meals
+(
+    meal_id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    date_time    TIMESTAMP NOT NULL,
+    description VARCHAR NOT NULL,
+    calories INTEGER NOT NULL
 );
