02a3b9db0f4e7c5ea80af92de036552b268ee95a
katrmin <katrmin@yandex.ru>
2021-11-03 23:53:08 +0300

    HW4 - delete @NotEmpty

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -12,7 +12,6 @@ import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
@@ -49,7 +48,6 @@ public class Meal extends AbstractBaseEntity {
     private int calories;
 
     @NotNull
-    @NotEmpty
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="user_id", nullable = false)
     private User user;
